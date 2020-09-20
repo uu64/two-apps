4adb843810741c3f824d698ab8c648c87295730e
uu64 <yusuke.t.0808@gmail.com>
2020-09-20 15:09:19 +0900

    fix: ui

--- two-front/pages/index.tsx
+++ two-front/pages/index.tsx
@@ -69,6 +69,7 @@ class Home extends React.Component<{}, State> {
     this.setState({
       message: "This is disconnected.",
     });
+    this.openSnackbar();
   }
 
   handleMessage(ev: MessageEvent): void {
